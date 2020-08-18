92ade3e1d06938a6ad00ab44381d7b40f28b00d6
Andrey Pechkurov <apechkurov@gmail.com>
2020-08-18 15:12:55 +0300

    Fix EE wrapper to patch listeners instead of emit

--- src/util.js
+++ src/util.js
@@ -1,14 +1,49 @@
 'use strict'
 
+const isWrappedSymbol = Symbol('cls-rtracer-is-wrapped')
+const wrappedSymbol = Symbol('cls-rtracer-wrapped-function')
+
+function wrapEmitterMethod (emitter, method, wrapper) {
+  if (emitter[method][isWrappedSymbol]) {
+    return
+  }
+
+  const original = emitter[method]
+  const wrapped = wrapper(original, method)
+  wrapped[isWrappedSymbol] = true
+  emitter[method] = wrapped
+
+  return wrapped
+}
+
+const addMethods = [
+  'on',
+  'addListener',
+  'prependListener'
+]
+
+const removeMethods = [
+  'off',
+  'removeListener'
+]
+
 /**
- * Monkey patches `.emit()` method of the given emitter, so
- * that all event listeners are run in scope of the provided
- * async resource.
+ * Wraps EventEmitter listener registration methods of the
+ * given emitter, so that all listeners are run in scope of
+ * the provided async resource.
  */
-const wrapEmitter = (emitter, asyncResource) => {
-  const original = emitter.emit
-  emitter.emit = function (type, ...args) {
-    return asyncResource.runInAsyncScope(original, emitter, type, ...args)
+function wrapEmitter (emitter, asyncResource) {
+  for (const method of addMethods) {
+    wrapEmitterMethod(emitter, method, (original) => function (name, handler) {
+      handler[wrappedSymbol] = asyncResource.runInAsyncScope.bind(asyncResource, handler, emitter)
+      return original.call(this, name, handler[wrappedSymbol])
+    })
+  }
+
+  for (const method of removeMethods) {
+    wrapEmitterMethod(emitter, method, (original) => function (name, handler) {
+      return original.call(this, name, handler[wrappedSymbol] || handler)
+    })
   }
 }
 
