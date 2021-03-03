9c1fa9d23a6c59fad8db70ac06d9c5294bad49cc
Serkan Özel <serkanozel380@gmail.com>
2021-03-03 20:09:35 +0300

    Add sample code for pino logger, used with fastify

---
+++ samples/pino.winston.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const rTracer = require('../index')
+
+// first - configure pino logger
+
+const logger = require('pino')({
+  mixin () {
+    return { request_id: rTracer.id() }
+  }
+})
+
+// next - configure and start Fastify app
+
+const Fastify = require('fastify')
+const app = new Fastify()
+
+app.register(rTracer.fastifyPlugin)
+
+app.get('/', async (request, reply) => {
+  logger.info('Starting request handling')
+  const result = await fakeDbAccess()
+  reply.send(result)
+})
+
+async function fakeDbAccess () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      logger.info('Logs from fakeDbAccess')
+      resolve({ message: 'Hello from cls-rtracer Fastify example' })
+    }, 0)
+  })
+}
+
+app.listen(3000, (err) => {
+  if (err) {
+    logger.error('The app could not start')
+  }
+  logger.info('The app is listening on 3000')
+})
